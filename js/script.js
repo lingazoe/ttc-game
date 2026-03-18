6f0cd8b5029b226dc4569db311fc8a527e50f625
zoe <160427655+lingazoe@users.noreply.github.com>
2026-03-18 12:12:43 -0400

    comm

--- js/script.js
+++ js/script.js
@@ -1,33 +1,53 @@
+
+//initialize the data
 const gameData = new data();
 
+//initalize the necessary HTML Elements
 const textDisplay = document.querySelector(".scrambled");
 const inputDisplay = document.querySelector(".guess");
 const answerBTN = document.querySelector("#answer");
 const nextBTN = document.querySelector("#next");
 const submitBTN = document.querySelector("#checking");
 
+//hide the [NEXT] button
 nextBTN.classList.add('hidden');
 
 async function start() {
 
+    //initialize game logic
     const gameLogic = new logic(gameData);
 
+    //load the csv
     await gameData.load();
 
+    //load other data
     gameLogic.setup();
 
-    //display answer and display the button
-    answerBTN.addEventListener('click', () => {
+    //reset values just in case...
+    gameLogic.reset();
 
-        const answer = gameLogic.getCurrentStationName();
+    /*
 
-        textDisplay.textContent = answer;
+    When the web page loads, user should be able to see:
 
-        nextBTN.classList.remove('hidden');
-        answerBTN.classList.add('hidden');
+    A scrambled text. Each time the website is refreshed, the website generates another
+    scrambled text.
+    
+    The input text box, with a black border. Has a sample station inside for example.
 
-        inputDisplay.classList.remove('green-border');
+    Has 2 buttons: Submit and Show Answer.
+    
+    */
+
+    //--- CLICKING THE 'SUBMIT' BUTTON ---//
+
+    submitBTN.addEventListener('click', () => {
+
+        const userGuess = inputDisplay.value;
+
+        textDisplay.textContent = userGuess;
     });
 }
 
+//start the game
 start();
